518dd6b21f58324f7e8b8e0231c4206c5d2b6662
manuel blanco <manuel.22.lamas@gmail.com>
2018-05-17 16:09:38 -0400

    Uploading files from a course finished a month ago

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const  AboutPage = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This aplication uses React, React router and a variaty of other helpful libraries</p>
+    </div>
+  );
+}
+
+
+export default AboutPage;
